1e5ee46534811ac83a1c241b84d9133fd8addb06
nyongmac <fomgilbert@gmail.com>
2023-12-27 04:16:04 -0500

    Jenkins modified

--- JenkinsLatestUbuntu.sh
+++ JenkinsLatestUbuntu.sh
@@ -12,8 +12,9 @@ sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sour
 curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee     /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 
 #Then add a Jenkins apt repository entry:
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]     https://pkg.jenkins.io/debian binary/ | sudo tee     /etc/apt/sources.list.d/jenkins.list > /dev/null
-
+eecho deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
 
 #Update your local package index, then finally install Jenkins:
 sudo apt-get update -y
